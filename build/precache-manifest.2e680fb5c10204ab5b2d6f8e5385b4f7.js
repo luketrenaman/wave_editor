e60688d5f48241c7fdaf8dd3f0ee8ed98534158f
BuildTools <trenamanluke@gmail.com>
2020-07-02 13:40:12 -0400

    I ignore the .gitignore

---
+++ build/precache-manifest.2e680fb5c10204ab5b2d6f8e5385b4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48635517fa2e9f6e1c3c8651a2d7e88c",
+    "url": "./build/index.html"
+  },
+  {
+    "revision": "d07de79e2113c0b106e4",
+    "url": "./build/static/css/main.d886a774.chunk.css"
+  },
+  {
+    "revision": "8b7705f95c5ebe054a2a",
+    "url": "./build/static/js/2.79243b34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./build/static/js/2.79243b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07de79e2113c0b106e4",
+    "url": "./build/static/js/main.d6166c3f.chunk.js"
+  },
+  {
+    "revision": "19097c47fed41c0388d2",
+    "url": "./build/static/js/runtime-main.6efa8bac.js"
+  }
+]);
